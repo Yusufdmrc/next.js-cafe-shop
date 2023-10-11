caf69803529a3227a80b63091d87adbf43429b61
yusuf <yusuffdemirci10@gmail.com>
2023-10-11 22:33:08 +0300

    search operations completed

--- components/customerProfile/Order.tsx
+++ components/customerProfile/Order.tsx
@@ -1,7 +1,44 @@
 import Title from "../ui/Title";
 import styles from "../../styles/customerProfile/order.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useSession } from "next-auth/react";
 
 const Order = () => {
+  const [orders, setOrders] = useState([]);
+  const [currentUser, setCurrentUser] = useState([]);
+
+  const { data: session } = useSession();
+  useEffect(() => {
+    const getOrders = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/orders`
+        );
+        setOrders(
+          res.data.filter((order) => order.customer === currentUser?.name)
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getOrders();
+  }, [currentUser]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/users`);
+        setCurrentUser(
+          res.data.filter((user) => user.email === session.user.email)[0]
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getUsers();
+  }, [session]);
+
   return (
     <div className={styles.form}>
       <Title>Hesap Ayarları</Title>
@@ -27,17 +64,19 @@ const Order = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className={styles.imageTr}>
-              <td className={styles.imageTd}>
-                <span>12124124214</span>
-              </td>
-              <td className={styles.extras}>
-                <span>İstanbul</span>
-              </td>
-              <td className={styles.price}>13/09/2023</td>
-              <td className={styles.amount}>200TL</td>
-              <td className={styles.status}>Hazırlanıyor</td>
-            </tr>
+            {orders.map((order) => (
+              <tr className={styles.imageTr} key={order?._id}>
+                <td className={styles.imageTd}>
+                  <span>12124124214</span>
+                </td>
+                <td className={styles.extras}>
+                  <span>İstanbul</span>
+                </td>
+                <td className={styles.price}>13/09/2023</td>
+                <td className={styles.amount}>200TL</td>
+                <td className={styles.status}>Hazırlanıyor</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
--- components/ui/SearchModal.tsx
+++ components/ui/SearchModal.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
 import OutsideClickHandler from "react-outside-click-handler";
 import Title from "../ui/Title";
 import styles from "../../styles/ui/SearchModal.module.css";
 import Image from "next/image";
 import { RiCloseCircleFill } from "react-icons/ri";
+import { useRouter } from "next/router";
+import axios from "axios";
+import Input from "../form/Input";
+
+interface Product {
+  _id: string;
+  title: string;
+  img: string;
+  prices: number[];
+}
 
 interface SearchModalProps {
   setIsModal: React.Dispatch<React.SetStateAction<boolean>>;
@@ -11,59 +21,76 @@ interface SearchModalProps {
 }
 
 const SearchModal: React.FC<SearchModalProps> = ({ setIsModal, isModal }) => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [filtered, setFiltered] = useState<Product[]>([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get<Product[]>(
+          `${process.env.NEXT_PUBLIC_API_URL}/products`
+        );
+        setProducts(res.data);
+        setFiltered(res.data.slice(0, 5));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    setTimeout(() => {
+      getProducts();
+    }, 1000);
+  }, []);
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const searchFilter = products
+      .filter((product) =>
+        product.title.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+      .slice(0, 5);
+    setFiltered(searchFilter);
+  };
+
   return (
     <div className={styles.searchModal}>
       <OutsideClickHandler onOutsideClick={() => setIsModal(!isModal)}>
         <div className={styles.grid}>
           <div className={styles.searchTitle}>
             <Title addClass={styles.hidden}>Search</Title>
-            <input
-              type="text"
-              placeholder="Arayın..."
-              className={styles.input}
-            />
+            <Input placeholder="Arayın" onChange={handleSearch} />
             <div>
-              <ul>
-                <li className={styles.searchContainer}>
-                  <div className={styles.searchImage}>
-                    <Image
-                      width={48}
-                      height={48}
-                      src="/images/discount.jpg"
-                      alt=""
-                      priority
-                    />
-                  </div>
-                  <span className={styles.span}>Donut</span>
-                  <span className={styles.span}>$10</span>
-                </li>
-                <li className={styles.searchContainer}>
-                  <div className={styles.searchImage}>
-                    <Image
-                      width={48}
-                      height={48}
-                      src="/images/campaign.jpg"
-                      priority
-                      alt=""
-                    />
-                  </div>
-                  <span className={styles.span}>Donut</span>
-                  <span className={styles.span}>$10</span>
-                </li>
-                <li className={styles.searchContainer}>
-                  <div className={styles.searchImage}>
-                    <Image
-                      width={48}
-                      height={48}
-                      src="/images/campaign.jpg"
-                      priority
-                      alt=""
-                    />
-                  </div>
-                  <span className={styles.span}>Donut</span>
-                  <span className={styles.span}>$10</span>
-                </li>
-              </ul>
+              {products.length > 0 ? (
+                <ul className={styles.ul}>
+                  {filtered.length > 0
+                    ? filtered.map((product) => (
+                        <li
+                          className={styles.searchContainer}
+                          key={product._id}
+                          onClick={() => {
+                            router.push(`/productDetail/${product._id}`);
+                            setIsModal(false);
+                          }}
+                        >
+                          <div className={styles.searchImage}>
+                            <Image
+                              width={48}
+                              height={48}
+                              src={product.img}
+                              alt={product.title}
+                              priority
+                            />
+                          </div>
+                          <span className={styles.span}>{product.title}</span>
+                          <span className={styles.span}>
+                            ${product.prices[0]}
+                          </span>
+                        </li>
+                      ))
+                    : "Ürün bulunamadı"}
+                </ul>
+              ) : (
+                ""
+              )}
               <a
                 onClick={() => setIsModal(!isModal)}
                 className={styles.closeIcon}
